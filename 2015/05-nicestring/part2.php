07e887e23d64f47b193644f867601b6d741e601d
Jeff Standen <jeff@webgroupmedia.com>
2022-12-05 13:08:42 -0800

    2015 Day 5: Doesn't He Have Intern-Elves For This?

---
+++ 2015/05-nicestring/part2.php
@@ -0,0 +1,20 @@
+<?php
+// https://adventofcode.com/2015/day/5#part2
+$strings = explode("\n", file_get_contents('data.txt'));
+
+echo count(array_filter($strings, function($string) {
+//    // Not at least three vowels
+//    if(count(array_intersect(str_split($string), ['a','e','i','o','u'])) < 3)
+//        return false;
+//
+//    // Not ab, cd, pq, xy
+//    foreach(['ab','cd','pq','xy'] as $forbidden)
+//        if(str_contains($string, $forbidden))
+//            return false;
+//
+//    // No repeat letters
+//    if(!preg_match('/([a-z])\1+/', $string))
+//        return false;
+
+    return true;
+})), PHP_EOL;
