12349f9d2755901f1f0fe98b295757d07a9d0143
Jeff Standen <jeff@webgroupmedia.com>
2022-12-29 19:07:33 -0800

    2015 Day 12: JSAbacusFramework.io (Part 1)

---
+++ 2015/12-json/part1.php
@@ -0,0 +1,9 @@
+<?php
+$json = json_decode(file_get_contents('input.json'), true);
+
+$numbers = [];
+array_walk_recursive($json, function($n) use (&$numbers) {
+	if(is_int($n)) $numbers[] = $n;
+});
+
+printf("Part 1: %d\n", array_sum($numbers));
